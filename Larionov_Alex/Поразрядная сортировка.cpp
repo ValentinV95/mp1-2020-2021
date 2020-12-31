74cb60cdd2afce3231247562067b0358c8a87be4
Alexej-Larionov <deervvv61@gmail.com>
2020-12-31 10:31:15 +0300

    Larionov_Alex

--- Larionov_Alex/Поразрядная сортировка.cpp
+++ Larionov_Alex/Поразрядная сортировка.cpp
@@ -1,17 +1,65 @@
 #include<iostream>
 #include<stdio.h>
-void countingSort(int* array, int* dest, int n, int m)
+#include<iostream>
+#include<stdio.h>
+void createCounters(double* data, int* counters, long N)
 {
-	int count[m+1] = { 0 };
-	int tmp1 = 0, tmp2 = 0;
-	for (int i = 0; i < n; i++)
-		count[array[i]]++;
-	tmp1 = count[0]; count[0] = 0;
-	for (int i = 1; i <= m; i++) {
-		tmp2 = count[i];       count[i] = count[i - 1] + tmp1;       tmp1 = tmp2;
+
+	unsigned char* bp = (unsigned char*)data;
+	unsigned char* dataEnd = (unsigned char*)(data + N);
+	unsigned short i;
+
+	memset(counters, 0, 256 * sizeof(double) * sizeof(long));
+
+	while (bp != dataEnd)
+	{
+		for (i = 0; i < sizeof(double); i++)
+			counters[256 * i + *(bp++)]++;
 	}
-	for (i = 0; i < n; i++) {
-		c = array[i];
-		dest[count[c]] = c;
-		count[c]++;
+}
+void radixPass(short Offset, long N, double* source, double* dest, int* count)
+{
+	double* sp;
+	int s = 0, c, i, * cp = count;
+	unsigned char* bp;
+
+	for (i = 256; i > 0; --i, ++cp)
+	{
+		c = *cp;		*cp = s;		s += c;
 	}
+	bp = (unsigned char*)source + Offset;
+	sp = source;
+	for (i = N; i > 0; --i, bp += sizeof(double), ++sp)
+	{
+		cp = count + *bp;	dest[*cp] = *sp;		(*cp)++;
+	}
+}
+void radixSort(double* array, long N)
+{
+	double* additional_array = (double*)malloc(N * sizeof(double));
+	int* counters = (int*)malloc(2048 * sizeof(int));
+	int* count;
+	int k = 0;
+	createCounters(array, counters, N);
+	for (unsigned short i = 0; i < sizeof(double); i++)
+	{
+		count = counters + 256 * i;
+		radixPass(i, N, array, additional_array, count);
+		for (int j = 0; j < N; j++)
+		{
+			array[j] = additional_array[j];
+		}
+	}
+	while (array[k] >= 0 && k < N && !(k > 0 && array[k] <= 0 && array[k - 1] > 0)) k++;
+	for (int i = 0; i < N - k; i++)
+	{
+		array[i] = additional_array[N - 1 - i];
+	}
+	for (int i = 0; i < k; i++)
+	{
+		array[N - k + i] = additional_array[i];
+	}
+	memcpy(array, additional_array, N * sizeof(double));
+	free(additional_array);
+	free(counters);
+}
