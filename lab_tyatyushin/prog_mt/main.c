be0f4b2b847b75fcf73529ac223087d0b6e0ad48
Maxim Tyatyushin <owl.gaze1003@gmail.com>
2021-01-04 19:58:52 +0300

    lab_tyatyushin

--- lab_tyatyushin/prog_mt/main.c
+++ lab_tyatyushin/prog_mt/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <locale.h>
+#include <math.h>
 
 void bubble_sort();
 
@@ -11,53 +12,58 @@ void merge();
 
 void mergeSort();
 
-void printArray();
-
 int get_max ();
 
 void radix_sort ();
+
+int compare ();
 
 double main()
 {
     int pr[1] = {0};
     int sr[1] = {0};
     int E;
-    int g;
-    int *mas;
-    double *arr;
+    int g,n;
+    int *mas, mas1[10];
+    double *arr, arr1[10];
     int i=0,j=0;
 
     setlocale(0,"");
-    printf("Введите число соответсвующее алгоритму сортировки\n1 - сортировка пузырьком\n2 - сортировка расчёской\n3 - сортировка слиянием\n4 - поразрядная сортировка\n");
+    printf("Введите число соответсвующее алгоритму сортировки\n1 - сортировка пузырьком\n2 - сортировка расчёской\n3 - сортировка слиянием\n4 - поразрядная сортировка\n5 - проверка\n");
     scanf("%d", &E);
     printf("Введите кол-во чисел массива: ");
     scanf("%d", &g);
-
-
     arr = (double*)malloc(g*sizeof(double));
-    mas = (int*)malloc(g*sizeof(int));
 
 
-    if(E==4)
+    if (E==4)
     {
-        for(i=0; i<g; i++) {
-        printf("mas[%d]=",i);
-        scanf("%d", &mas[i]);
+        printf("Введите элементы массива: ");
+        for(i=0;i<g;i++){
+        scanf("%lf", &arr1[i]);
         }
     }
     else
     {
-        for(i=0; i<g; i++) {
-        printf("arr[%d]=",i);
+        printf("Введите элементы массива: ");
+        for(i=0;i<g;i++){
         scanf("%lf", &arr[i]);
         }
+    }
+
+    if (E==4)
+    {
+
+        printf ("Введите точность: ");
+        scanf ("%d", &n);
     }
 
     if (E==1)
     bubble_sort(arr, g);
     if (E==2)
     comb_sort(arr, g);
-    if (E==3){
+    if (E==3)
+    {
         mergeSort(arr, 0, g - 1, sr, pr);
         printf("\n");
         printf("Количество перестановок: %d\n", pr[0]);
@@ -65,25 +71,43 @@ double main()
     }
     if (E==4)
     {
-    radix_sort (mas, g, sr, pr);
-    printf("\n");
+        for(i=0; i<g; i++){
+            mas1[i]=(int)(arr1[i]*pow(10,n));
+
+        }
+        radix_sort (mas1, g, pr);
+        for(i=0; i<g; i++){
+            arr1[i]=(double)(mas1[i]*pow(10,-n));
+
+        }
+        printf("\n");
         printf("Количество перестановок: %d\n", pr[0]);
-        printf("Количество сравнений : %d\n", sr[0]);
     }
+    if(E==5) qsort(arr, g, sizeof(double), compare);
 
-
-    if(E==4)
+    if (E==4)
     {
         for(i=0 ; i<g ;  i++)
-        {
-        printf("%d ",mas[i]);
-        }
+        printf("%lf ",arr1[i]);
     }
     else
     {
         for(i=0 ; i<g ;  i++)
         printf("%lf ",arr[i]);
     }
+
+    if(E==5){
+        printf("\nAfter sorting the list is: ");
+        for( i = 0 ; i < g; i++ )
+        {
+            printf("%lf ", arr[i]);
+        }
+
+    }
+
+    printf("\n");
+
+    return(0);
 
 }
 
@@ -143,31 +167,27 @@ void merge(double arr[], int l, int m, int r, int *s, int *p)
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    double L[n1], R[n2];
+    double Left[n1], Right[n2];
 
     for (i = 0; i < n1; i++){
-        L[i] = arr[l + i];
-        p[0]++;
-        s[0]++;
+        Left[i] = arr[l + i];
     }
     for (j = 0; j < n2; j++){
-        R[j] = arr[m + 1 + j];
-        p[0]++;
-        s[0]++;
+        Right[j] = arr[m + 1 + j];
     }
 
     i = 0;
     j = 0;
     k = l;
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
+        if (Left[i] <= Right[j]) {
+            arr[k] = Left[i];
             i++;
             p[0]++;
             s[0]++;
         }
         else {
-            arr[k] = R[j];
+            arr[k] = Right[j];
             j++;
             p[0]++;
             s[0]++;
@@ -176,14 +196,14 @@ void merge(double arr[], int l, int m, int r, int *s, int *p)
     }
 
     while (i < n1) {
-        arr[k] = L[i];
+        arr[k] = Left[i];
         i++;
         k++;
         p[0]++;
     }
 
     while (j < n2) {
-        arr[k] = R[j];
+        arr[k] = Right[j];
         j++;
         k++;
         p[0]++;
@@ -205,57 +225,52 @@ void mergeSort(double arr[], int l, int r, int *s, int *p)
 }
 
 
-void printArray(double A[], int size)
-{
 
-    for (int i = 0; i < size; i++)
-        printf("%lf ", A[i]);
-    printf("\n");
-}
 
-int get_max (int arr[], int g, int *s, int *p){
+int get_max (int arr[], int g, int *p){
    int max = arr[0];
    for (int i = 1; i < g; i++)
       if (arr[i] > max){
          max = arr[i];
          p[0]++;
-         s[0]++;
       }
-      s[0]++;
    return max;
 }
-void radix_sort (int arr[], int g, int *s, int *p){
-   int bucket[10][10], bucket_cnt[10];
-   int i, j, k, r, NOP = 0, divisor = 1, lar, pass;
-   lar = get_max (arr, g, s, p);
-   while (lar > 0){
-      NOP++;
-      lar /= 10;
-      s[0]++;
+void radix_sort (int arr[], int g, int *p){
+   int yas[10][10], yas_cnt[10];
+   int i, j, k, r, ne = 0, divisor = 1, l, pss;
+   l = get_max (arr, g, p);
+   while (l > 0){
+      ne++;
+      l /= 10;
    }
-   for (pass = 0; pass < NOP; pass++){
+   for (pss = 0; pss < ne; pss++){
       for (i = 0; i < 10; i++){
-         bucket_cnt[i] = 0;
+         yas_cnt[i] = 0;
          p[0]++;
-         s[0]++;
+
       }
       for (i = 0; i < g; i++){
          r = (arr[i] / divisor) % 10;
-         bucket[r][bucket_cnt[r]] = arr[i];
-         bucket_cnt[r] += 1;
+         yas[r][yas_cnt[r]] = arr[i];
+         yas_cnt[r] += 1;
          p[0]++;
-         s[0]++;
+
       }
       i = 0;
       for (k = 0; k < 10; k++){
-         for (j = 0; j < bucket_cnt[k]; j++){
-            arr[i] = bucket[k][j];
+         for (j = 0; j < yas_cnt[k]; j++){
+            arr[i] = yas[k][j];
             i++;
             p[0]++;
-            s[0]++;
          }
       }
       divisor *= 10;
    }
 }
-
+int compare (const void * a, const void * b)
+{
+    if (*(double*)a > *(double*)b) return 1;
+    else if (*(double*)a < *(double*)b) return -1;
+    else return 0;
+}
