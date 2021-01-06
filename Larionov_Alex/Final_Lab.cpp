911180ee381521ed99efe9706cebd6b46e0643c5
Alexej-Larionov <76392422+Alexej-Larionov@users.noreply.github.com>
2021-01-06 19:15:22 +0300

    Add files via upload
    Поправил и проверил поразрядную сортировку, дополнил функцию создания массива опцией введения массива вручную, изменил первую опцию заполнения массива(теперь генерируются числа разные по знаку, целой и дробной частям), совершил мелкие косметические изменения в коде.

---
+++ Larionov_Alex/Final_Lab.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include"Lab_sorts.h"
+#include<stdio.h>
+#include <math.h>
+int main()
+{
+	srand(time(NULL));
+	int a, b, i, c, g, last = 0;
+	double* massive, d = 0, l = 0;
+	printf("Choose massive's length -");
+	scanf("%d", &a);
+	massive = (double*)malloc(a * sizeof(double));
+	if (massive == NULL)
+	{
+		printf("Произошла ошибка. Запросили слишком много памяти??\n");
+		return -1;
+	}
+	printf("For massive of random floating point numbers press - 1\n For massive of ordered floating point numbers press - 2\n For massive of reverse ordered floating point numbers press - 3\n To insert your own massive press - 4\n");
+	scanf("%d", &b);
+	switch (b)
+	{
+	case 1:
+	{printf("Massive of random floating point numbers.\n");
+	for (i = 0; i < a; i++)
+	{
+		massive[i] = pow(-1,i)*rand()+((double)rand() / (double)((RAND_MAX) + 1));
+		printf("%lf\n", massive[i]);
+	}
+	break;
+	}
+	case 2:
+	{
+		printf("Massive of ordered floating point numbers.\n");
+		for (i = 0; i < a; i++)
+		{
+			d++;
+			massive[i] = d + ((double)rand() / (double)(RAND_MAX + 1));
+			printf("%f\n", massive[i]);
+		}
+		break;
+	}
+	case 3:
+	{
+		printf("Massive of reverse ordered floating point numbers.\n");
+		for (i = 0; i < a; i++)
+		{
+			d++;
+			massive[i] = -1 * d + (double)(rand() / (double)(RAND_MAX + 1));
+			printf("%f\n", massive[i]);
+		}
+		break;
+	}
+	case 4: {for (i = 0; i < a; i++) { printf("Massive element number %d = ",i); scanf("%lf", &l); massive[i] = l; }}
+	}
+	printf("For insertion sort press - 1\n For Shell's sort press - 2\n For radix sort press - 3\n For merge sort press - 4\n");
+	scanf("%d", &c);
+	switch (c)
+	{
+	case 1: {insertion_sort(massive, a); break; }
+	case 2: {Shell_sort(massive, a); break; }
+	case 3: {radixSort(massive, a); break; }
+	case 4:
+	{
+		double* spare = (double*)malloc(a * sizeof(double));
+		mergeSort(massive, spare, 0, a - 1);
+	}
+	break;
+	}
+	for (i = 0; i < a; i++)
+	{
+		printf("%f\n", massive[i]);
+	}
+	printf("\nPress 1 to check for sort outcome errors\n");
+	scanf("%d", &g);
+	if (g == 1)
+	{
+		for (i = 0; i < a - 1; i++)
+		{
+			if (massive[i] > massive[i + 1])
+			{
+				printf("sort went wrong\n", "%d%f%d%f", "massive element ", i, " = ", massive[i], "massive element ", i + 1, " = ", massive[i + 1]);
+			}
+			else
+			{
+				last = last + 1;
+			}
+		}
+	}
+	if (last != 0) { printf("Sort is Ok"); }
+	return(0);
+}
---
+++ Larionov_Alex/Lab_sorts.h
@@ -0,0 +1,148 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+int swaps=0, comps=0;
+void insertion_sort(double arr[], int n)
+{
+	int a = 0; int b = 0;
+	for (int i = 1; i < n; i++)
+		for (int j = i; j > 0 && arr[j - 1] >= arr[j]; j--)
+		{
+			double tmp = arr[j - 1];
+			arr[j - 1] = arr[j];
+			arr[j] = tmp;
+			swaps++; comps++;
+		}
+	printf("swaps = %d  comparisons = % d\n", swaps, comps);
+}
+void Shell_sort(double array[], const int n)
+{
+	int step, i, j,a = 0,b = 0;
+	double tmp;
+	for (step = n / 2; step > 0; step /= 2)
+		for (i = step; i < n; i++)
+			for (j = i - step; j >= 0 && array[j] > array[j + step]; j -= step)
+			{
+				tmp = array[j];
+				array[j] = array[j + step];
+				array[j + step] = tmp;
+				swaps++; comps++;
+			}
+	printf("swaps = %d  comparisons = % d\n", swaps, comps);
+}
+void createCounters(double* data, int* counters, long N)
+{
+
+	unsigned char* bp = (unsigned char*)data;
+	unsigned char* dataEnd = (unsigned char*)(data + N);
+	unsigned short i;
+
+	memset(counters, 0, 256 * sizeof(double) * sizeof(int));
+
+	while (bp != dataEnd)
+	{
+		for (i = 0; i < sizeof(double); i++)
+			counters[256 * i + *(bp++)]++;
+	}
+}
+void radixPass(short Offset, long N, double* source, double* dest, int* count)
+{
+	double* sp;
+	int s = 0, c, i, * cp = count;
+	unsigned char* bp;
+
+	for (i = 256; i > 0; --i, ++cp)
+	{
+		c = *cp;		*cp = s;		s += c;
+	}
+	bp = (unsigned char*)source + Offset;
+	sp = source;
+	for (i = N; i > 0; --i, bp += sizeof(double), ++sp)
+	{
+		cp = count + *bp;	dest[*cp] = *sp;	(*cp)++;    swaps++;
+	}
+}
+void radixSort(double* array, long N)
+{
+	double* additional_array = (double*)malloc(N * sizeof(double));
+	int* counters = (int*)malloc(2048 * sizeof(int));
+	int* count;
+	int k = 0, a = 0, b = 0;
+	createCounters(array, counters, N);
+	for (unsigned short i = 0; i < sizeof(double); i++)
+	{
+		count = counters + 256 * i;
+		radixPass(i, N, array, additional_array, count);
+		for (int j = 0; j < N; j++)
+		{
+			array[j] = additional_array[j]; a++;
+		}
+	}
+	while (array[k] >= 0 && k < N && !(k > 0 && array[k] <= 0 && array[k - 1] > 0)) { comps=comps+3; k++; }
+	for (int i = 0; i < N - k; i++)
+	{
+		array[i] = additional_array[N - 1 - i];  swaps++;
+	}
+	for (int i = 0; i < k; i++)
+		{
+		array[N - k + i] = additional_array[i]; swaps++;
+		}
+	printf("swaps = %d  comparisons = % d\n", swaps,  comps);
+}
+void merge(double array[], double spare[], int l, int r)
+{
+	int i = l, q = (l + r) / 2, j = q + 1, k = 0;
+	while (i <= q && j <= r)
+	{
+		if (array[i] <= array[j])
+		{
+			swaps++;
+			comps++;
+			spare[k] = array[i];
+			i++;
+		}
+		else
+		{
+			swaps++;
+			comps++;
+			spare[k] = array[j];
+			j++;
+		}
+		k++;
+	}
+	while (i <= q)
+	{
+		swaps++;
+		spare[k] = array[i];
+		i++; k++;
+	}
+	while (j <= r)
+	{
+		swaps++;
+		spare[k] = array[j];
+		j++;
+		k++;
+	}
+	for (i = 0; i < k; i++)
+	{
+		swaps++;
+		array[l + i] = spare[i];
+	}
+}
+int merge_sort(double array[], double spare[], int l, int r)
+{
+	if (l < r)
+	{
+		int q = (l + r) / 2;
+		merge_sort(array, spare, l, q);
+		merge_sort(array, spare, q + 1, r);
+		merge(array, spare, l, r);
+	}
+	return(0);
+}
+int mergeSort(double array[], double spare[], int l, int r)
+{
+	merge_sort(array, spare, l, r);
+	printf("swaps = %d  comparisons = % d\n", swaps, comps);
+	return(0);
+}
