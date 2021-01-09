70f5a1a0e185d38d50a8ba5fc7362a788855051a
IzmaylovI <ilshat.izmaylov.03@mail.ru>
2021-01-09 20:12:00 +0300

    TMaksimLaba

--- IvlevAD/1-st Lab/TestSort.h
+++
@@ -1,6 +0,0 @@
-#ifndef TESTSORT_H
-#define TESTSORT_H
-
-void TestSort(unsigned int, float*, float*);
-
-#endif
--- MitinRA/laboratornaia/laboratornaia/rabotasmassivami.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-void sozdaniemassiva(float* a, int N);
-void vivodmassiva(float* a, int N);
-void vivodmassiva2(float* a, int N);
-void zapolnenievrucnuu(float* a, int N);
-int proverkamassiva(float* a, float* b, int N);
-void arraycopy(float* a, float* b, int N);
