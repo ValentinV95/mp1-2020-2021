02c01a5a24bc97f99d311e1007978bd4ead19dd5
Alexej-Larionov <deervvv61@gmail.com>
2021-05-02 08:43:15 +0300

    upd

--- Larionov_Alex/Labwork_2/VectorLab/VectorLab.cpp
+++ Larionov_Alex/Labwork_2/VectorLab/VectorLab.cpp
@@ -4,42 +4,41 @@ template <class T>
 class vector
 {
 public:
-	T* link, input; int size;
-	vector<T>(int size1)
+	T* link, input; size_t size;
+	vector<T>() { size = 0; link = 0; input = 0; };
+	vector<T>(size_t size1)
 	{
 		size = size1;
 		input = 0;
 		link = new T[size];
-		for (int i = 0; i < (this->size); i++)
-		{
-			link[i] = 0;
-		}
 	}
 	void fill_manually(vector A)
 	{
-		T* link1 = A.getlink();
-		for (int i = 0; i < (this->size); i++)
+		for (size_t i = 0; i < (this->size); i++)
 		{
 			cin >> input;
-			link1[i] = input;
+			A.link[i] = input;
 		}
 	}
 	void fill_randomly(vector A)
 	{
 		srand(time(NULL));
-		T* link1 = A.getlink();
-		for (int i = 0; i < (this->size); i++)
+		for (size_t i = 0; i < (this->size); i++)
 		{
-			link1[i] = rand();
+			A.link[i] = rand();
 		}
 	}
-	int getsize() { return(size); }
-	T* getlink() { return(link); }
+	size_t getsize() { return(size); }
+	T& getlink(size_t i) { return(link[i]); }
+	void setlink_manual(size_t i)
+	{
+		cin >> this->link[i];
+	}
 	//~vector(){ delete[] link;}
 	friend vector<T> operator + (vector& left, vector& right)
 	{
-		vector<T> temp(left.getsize());
-		for (int i = 0; i < temp.size; i++)
+		vector<T> temp(left.size);
+		for (size_t i = 0; i < temp.size; i++)
 		{
 			temp.link[i] = 0;
 			temp.link[i] = left.link[i] + right.link[i];
@@ -48,8 +47,8 @@ public:
 	}
 	friend vector<T> operator - (vector& left, vector& right)
 	{
-		vector<T> temp(left.getsize());
-		for (int i = 0; i < temp.size; i++)
+		vector<T> temp(left.size);
+		for (size_t i = 0; i < temp.size; i++)
 		{
 			temp.link[i] = 0;
 			temp.link[i] = left.link[i] - right.link[i];
@@ -58,8 +57,8 @@ public:
 	}
 	friend vector<T> operator * (T& left, vector& right)
 	{
-		vector temp(right.getsize());
-		for (int i = 0; i < temp.size; i++)
+		vector temp(right.size);
+		for (size_t i = 0; i < temp.size; i++)
 		{
 			temp.link[i] = 0;
 			temp.link[i] = left * right.link[i];
@@ -68,22 +67,22 @@ public:
 	}
 	friend vector<T> operator / (vector& left, T& right)
 	{
-		vector temp(left.getsize());
-		for (int i = 0; i < temp.size; i++)
+		vector temp(left.size);
+		for (size_t i = 0; i < temp.size; i++)
 		{
 			temp.link[i] = 0;
 			temp.link[i] = (left.link[i]) / right;
 		}
 		return(temp);
 	}
-	//vector<T>& operator[](int i)	{if (i > size){			cerr << "Index out of range";			exit(0);		}		return link[i];}
-	vector operator = (vector<T>* left)
+	vector<T>& operator = (vector right)
 	{
-		this->size = left.size;	
-		for (int i = 0; i < size; i++) 
-		{ 
-			this->link[i] = left.link[i]; 
+		vector<T> temp(right.size);
+		for (size_t i = 0; i < temp.size; i++)
+		{
+			temp.link[i] = right.link[i];
 		}
+		return(temp);
 	}
 	void print()
 	{
@@ -94,33 +93,33 @@ template <class T>
 class matrix :public vector<vector<T>>
 {
 public:
-	matrix<T>(int size2):vector<vector<T>>(size2)
+	matrix<T>(size_t size1)
 	{
-		this->size = size2;
-		this->link = new vector<vector<T>>(this->size);
-		for (int i = 0; i < this->size; i++)
+		this->size = size1;
+		vector<T> A(this->size);
+		for(size_t i=0; i<this->size; i++)
 		{
-			this->link[i] = new vector<T>(this->size);
+			this->link[i] = A;
 		}
 	}
-	void printM(matrix a)
+	void printm()
 	{
-		T b;
-		for (int i = 0; i < a.getsize(); i++)
+		for (size_t i = 0; i < this->size; i++)
 		{
-			for (int j = 0; j < a.getsize(); j++)
-			{
-				b = this->link[i][j];
-				cout << "link[" << i << "][" << j << "]=" << b << endl;
-			}
+			this->link[i].print();
+		}
+	}
+	void fillm_manuall()
+	{
+		for(size_t i=0; i< this->size; i++)
+		{
+			this->link[i].setlink_manuall(i);
 		}
 	}
 };
 int main()
 {
-	//vector<double> C(10), B(10), A(10);	vector<double> D(5);A.fill_manually(A);	B.fill_manually(B);	int a = 5;	double b = 4.5;	C = b * A;	cout << "C=";	C.print();	cout << "b=" << b << endl;	C = C / b;	//A = B - C;	cout << "C=";	C.print();	cout << endl; cout << "A=";	A.print();
-	vector<int> B(2);
-	matrix<int> A(10);
-	//A.printM(A);
-
+	matrix<int> A(5);
+	A.fillm_manuall();
+	A.printm();
 };
